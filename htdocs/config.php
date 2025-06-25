f5cc520d1afc3fba26ccdf4fceb3b7bd14bb6f69
SealGummies <xiangchitaozi@gmail.com>
2025-06-25 15:53:00 -0700

    uploaded config.php file

---
+++ htdocs/config.php
@@ -0,0 +1,11 @@
+<?php
+// Database configuration
+define('DB_HOST', 'your_host_here');
+define('DB_USER', 'your_username_here');
+define('DB_PASS', 'your_password_here');
+define('DB_NAME', 'your_database_name_here');
+
+// Other options
+define('SITE_NAME', 'KartRider Database Viewer');
+define('DEBUG_MODE', false);
+?>
